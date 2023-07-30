b016a507f9cde04c031ec9fc2254c056db2029f5
Risto Hietala <risto@hietala.org>
2023-07-30 12:51:52 +0300

    First version of the game

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# theseus-and-the-minotaur
-Theseus and the minotaur maze game in CLI
+# Theseus and the Minotaur
+
+[Theseus and the minotaur](https://en.wikipedia.org/wiki/Theseus_and_the_Minotaur)
+maze game in CLI. The idea is that you must escape a maze while minotaur is
+chasing you.
+
+Minotaur moves twice and it must move horizontally first if that is possible.
+
+---
+
+Requirements: python 3 and `getch` library.
+
+```sh
+$ pip install getch
+```
+
+Characters:
+
+- `*` player
+- `M` minotaur
+- `#` wall
+
+Controls:
+
+- `n`: north
+- `s`: south
+- `e`: east
+- `w`: west
+- `d`: delay, skip your move
+
+Starting the game:
+
+```sh
+$ python3 theseus-and-the-minotaur.py maze1.txt
+
+
+#############################
+#. . . . .#. . . .#. . . . .#
+##### # # # ### ##### ##### #
+#.#.#.#.#.#.#. . . . .#. . .
+# # # ### # ### ########### #
+#. . . . . . . . . . . . . .#
+# # ### # # ### # # ##### # #
+#.#.#. .#.#. . .#.#. . . .#.#
+# # ### # # # # # ####### # #
+#*#.#. .#.#.#.#.#. . . .#.#M#
+# # # # # # # # # ##### # # #
+#.#.#.#. .#.#. . . . . .#.#.#
+# # # # # # # # # ##### # # #
+#.#.#.#.#.#.#.#.#. . . .#.#.#
+# # # # # # # # # ##### # # #
+#.#.#.#.#.#.#.#.#. . . .#.#.#
+# # # # # # # # # ##### # # #
+#. . . . . . . . . . . . . .#
+#############################
+```
---
+++ theseus-and-the-minotaur.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+
+from typing import List, Tuple, NamedTuple
+from copy import deepcopy
+from getch import getch  # type: ignore
+import sys
+
+EMPTY = " "
+WALL = "#"
+WALKABLE = "."
+FINISH = "X"
+
+PLAYER = "*"
+MINOTAUR = "M"
+
+MOVE_UP = "n"
+MOVE_DOWN = "s"
+MOVE_LEFT = "w"
+MOVE_RIGHT = "e"
+MOVE_SKIP = "d"
+MOVE_QUIT = "q"
+
+
+Maze = List[List[str]]
+Coord = Tuple[int, int]
+
+
+class State(NamedTuple):
+    maze: Maze
+    player: Coord
+    minotaur: Coord
+    finish: Coord
+    moves: List[str]
+
+
+def loadMaze(filename: str) -> State:
+    """
+    Loads a maze from a file
+    """
+    maze: Maze = []
+    player: Coord = (0, 0)
+    minotaur: Coord = (0, 0)
+    finish: Coord = (0, 0)
+
+    with open(filename) as f:
+        for (y, line) in enumerate(f):
+            maze.append([])
+            for (x, char) in enumerate(line.strip()):
+                if char == PLAYER:
+                    player = (x, y)
+                    char = WALKABLE
+                elif char == MINOTAUR:
+                    minotaur = (x, y)
+                    char = WALKABLE
+                elif char == FINISH:
+                    finish = (x, y)
+                    char = EMPTY
+
+                maze[y].append(char)
+
+    return State(maze, player, minotaur, finish, [])
+
+
+def printMaze(state: State) -> None:
+    """
+    Prints the maze with the player and minotaur
+    """
+    maze, player, minotaur, _, _ = state
+    printedMaze: Maze = deepcopy(maze)
+    printedMaze[player[1]][player[0]] = PLAYER
+    printedMaze[minotaur[1]][minotaur[0]] = MINOTAUR
+
+    print("\033c", end="")  # clear screen
+    for row in printedMaze:
+        print("".join(row))
+
+
+def isLocValid(maze: Maze, loc: Coord) -> bool:
+    """
+    Checks if a location is valid in the maze
+    """
+    x, y = loc
+    if y < 0 or y >= len(maze) or x < 0 or x >= len(maze[y]):
+        return False
+    if maze[y][x] == WALL:
+        return False
+
+    return True
+
+
+def movePlayer(maze: Maze, player: Coord, move: str) -> Tuple[Coord, bool]:
+    """
+    Player or minotaur moves in the maze
+
+    Returns the new location and whether the move was valid.
+    """
+    x, y = player
+    if move == MOVE_UP:
+        newPlayer = (x, y - 2)
+        validCheckLoc = (x, y - 1)
+    elif move == MOVE_DOWN:
+        newPlayer = (x, y + 2)
+        validCheckLoc = (x, y + 1)
+    elif move == MOVE_LEFT:
+        newPlayer = (x - 2, y)
+        validCheckLoc = (x - 1, y)
+    elif move == MOVE_RIGHT:
+        newPlayer = (x + 2, y)
+        validCheckLoc = (x + 1, y)
+    elif move == MOVE_SKIP:
+        return (player, True)
+    else:
+        return (player, False)
+
+    if isLocValid(maze, validCheckLoc):
+        return (newPlayer, True)
+    else:
+        return (player, False)
+
+
+def moveMinotaur(state: State) -> Coord:
+    """
+    Minotaur moves towards player
+
+    It must move horizontally first and then vertically.
+    """
+    maze, player, minotaur, _, _ = state
+    move = MOVE_SKIP
+
+    # try to move horizontally
+
+    if player[0] < minotaur[0]:
+        move = MOVE_LEFT
+    elif player[0] > minotaur[0]:
+        move = MOVE_RIGHT
+
+    (newMinotaur, _) = movePlayer(maze, minotaur, move)
+    if newMinotaur != minotaur:
+        return newMinotaur
+
+    # try to move vertically
+
+    if player[1] < minotaur[1]:
+        move = MOVE_UP
+    elif player[1] > minotaur[1]:
+        move = MOVE_DOWN
+
+    (newMinotaur, _) = movePlayer(maze, minotaur, move)
+    return newMinotaur
+
+
+def main(filename: str):
+    """
+    Main game loop
+
+    - print initial maze
+    - wait for input
+    - check if move is valid
+    - move player
+    - move minotaur twice
+    - check for winning and losing conditions
+    """
+    state = loadMaze(filename)
+
+    while True:
+        printMaze(state)
+        move = getch()
+
+        if move == MOVE_QUIT:
+            print("You quit! Moves so far:")
+            print(";".join(state.moves))
+            sys.exit(0)
+
+        (player, valid) = movePlayer(state.maze, state.player, move)
+        if not valid:
+            continue
+
+        state = state._replace(player=player)
+        state = state._replace(moves=state.moves + [move])
+
+        # minotaur moves twice
+        minotaur = moveMinotaur(state)
+        state = state._replace(minotaur=minotaur)
+        minotaur = moveMinotaur(state)
+        state = state._replace(minotaur=minotaur)
+
+        if player == state.minotaur:
+            printMaze(state)
+            print("You lose! Moves:")
+            print(";".join(state.moves))
+            sys.exit(1)
+
+        if player == state.finish:
+            printMaze(state)
+            print("You win! Moves:")
+            print(";".join(state.moves))
+            sys.exit(0)
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 theseus-and-the-minotaur.py <maze filename>")
+    sys.exit(1)
+
+main(sys.argv[1])
